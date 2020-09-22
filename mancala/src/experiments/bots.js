346ddefd340e5806492f31e2f2306dee5f332fb3
slior <lior.schejter@gmail.com>
2020-09-22 17:56:49 +0300

    adding the option for game size in the bot script

--- mancala/src/experiments/bots.js
+++ mancala/src/experiments/bots.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 
 
 const DEFAULT_ROUND_COUNT = 50;
+const DEFAULT_GAME_SIZE = 14;
 
 function gameMsg(s) { }
 console.debug = s => { }
@@ -17,17 +18,20 @@ program
   .option('-p2, --player2 <AI 2 type>', "The bot to use for player 2")
   .option('-r, --rounds <rnds>', `Number of games to play. Defaults to ${DEFAULT_ROUND_COUNT}`)
   .option('-o, --out <dir>', "Output filename. Optional; if not specified one will be generated.")
+  .option('-s, --size <number>',`Size of the mancala game to play. An even integer number in the range of 6-30. Optional with a default of ${DEFAULT_GAME_SIZE}.`)
   .parse(process.argv)
 
 requires(program.player1,"Player 1 bot must be specified")
 requires(program.player2,"Player 2 bot must be specified")
 
 let roundsToRun = (program.rounds || DEFAULT_ROUND_COUNT)*1;
-let outputFilename = program.out || `p1_${program.player1.toLowerCase()}__p2_${program.player2.toLowerCase()}__${roundsToRun}.csv`
+let gameSize = (program.size || DEFAULT_GAME_SIZE)*1;
+let outputFilename = program.out || `size-${gameSize}__p1-${program.player1.toLowerCase()}__p2-${program.player2.toLowerCase()}__${roundsToRun}.csv`
 
 say(`Player 1 type: ${program.player1}`)
 say(`Player 2 type: ${program.player2}`)
 say(`Rounds: ${roundsToRun}`)
+say(`Game size: ${gameSize}`)
 say(`Output to: ${outputFilename}`)
 
 
@@ -35,7 +39,7 @@ say(`Output to: ${outputFilename}`)
 range(1,roundsToRun).forEach(round => {
     dbg(`Round ${round}`)
     let startTime = Date.now();
-    let game = new MancalaGame(14,'',_ => {},gameMsg,{p1 : program.player1, p2:program.player2},results => {
+    let game = new MancalaGame(gameSize,'',_ => {},gameMsg,{p1 : program.player1, p2:program.player2},results => {
         let endTime = Date.now();
         dbg (`Round ${round} Results: ${JSON.stringify(results)} Time: ${endTime-startTime}`)
         let line = []
